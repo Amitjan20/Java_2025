dc7bdfa93d8258548ff0d1442b43f78f6a4e21e4
amitjan20 <aslekaramit@gmail.com>
2025-04-27 09:14:29 -0700

    Create Interval.java

---
+++ Interval.java
@@ -0,0 +1,30 @@
+package jan2025;
+
+public class Interval {
+	
+	int start;
+	int end;
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public Interval(int start, int end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
+	public Interval() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
